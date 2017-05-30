e73d9805ace93d9f69961ff03dbd4e47e2cd2147
lab533 <laboratory533@gmail.com>
2017-05-30 17:20:20 +0300

    added files

---
+++ src/feature_makers.py
@@ -0,0 +1,152 @@
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+
+
+
+
+def xpressions_to_order_matrix(xpressions):
+    orders_num = np.arange(xpressions.shape[1], dtype=np.int32)
+    order_matrix = np.zeros_like(xpressions, dtype=np.int32)
+    for row in range(xpressions.shape[0]):
+        ordered_ids = np.array([ind
+                                for ind, xpr in sorted([(ind, xpr)
+                                                        for ind, xpr in enumerate(xpressions[row, :])],
+                                                       key=lambda p: p[1])])
+        order = np.zeros_like(ordered_ids, dtype=np.int32)
+        order[ordered_ids] = np.arange(xpressions.shape[1])
+        order_matrix[row, :] = order
+    return order_matrix
+
+
+def xpression_to_order_feature(xpressions, pairs, gens):
+    order_features = np.zeros(shape=(xpressions.shape[0], len(pairs)))
+    features_name = []
+    for idx, pair in enumerate(pairs):
+        order_features[:, idx] = xpressions[:, pair[0]] > xpressions[:, pair[1]] + 0
+        features_name.append("compare_expr({}, {})".format(gens[pair[0]], gens[pair[1]]))
+    return order_features, features_name
+
+
+def get_n_biggest_sims_ids(sims, subset, n=140):
+    subsims = sims[subset]
+    bound = np.sort(subsims)[-n]
+    return subset[subsims >= bound][:n]
+
+
+def get_gen_hists(xpression, cl_masks, bins=10):
+    xpr_range = (xpression.min(), xpression.max())
+    hists = []
+    for mask in cl_masks:
+        hist, _ = np.histogram(xpression[mask], bins=bins, range=xpr_range)
+        hists.append(hist)
+    return hists
+
+
+def get_pdf_sim(p, q):
+    return -q.dot(np.log2(p)) - p.dot(np.log2(q))
+
+def heuristic_1_2_features(X, y, train, test):
+    description = "экспрессии генов отобранных по пересечению 1-й и второй эвристик\n id генов:\n"
+    gen_res = []
+    for g in range(X.shape[1]):
+        lg = LogisticRegression(penalty='l1', C=1)
+        lg.fit(X[train, g:g + 1], y[train])
+        gen_res.append((lg.predict(X[train, g:g + 1]) == y[train]).mean())
+    gen_res = np.array(gen_res)
+    good_h1 = np.arange(X.shape[1])[gen_res > y[train].mean()]
+
+    gen_corr = []
+    for g in range(X.shape[1]):
+        gen_corr.append(abs(np.corrcoef(X[train, g], y[train])[0, 1]))
+    gen_corr = np.array(gen_corr)
+    good_h2 = np.arange(X.shape[1])[gen_corr > np.percentile(gen_corr, 98.2)]
+    good = np.array([g for g in set(good_h1).intersection(set(good_h2))])
+    # print(good)
+    description = description + "\t".join([str(gid) for gid in good])
+    return X[:, good], description
+
+
+def get_pair_value(order_matrix, pair, responses, pr=None):
+    if not pr:
+        pr = (responses == 1).mean()
+    p_ab = np.mean(order_matrix[:, pair[0]] > order_matrix[:, pair[1]])
+    p_ab_r = np.mean(order_matrix[responses == 1, pair[0]] > order_matrix[responses == 1, pair[1]])
+    pr_ab = (p_ab_r * pr) / p_ab
+    p_ba = np.mean(order_matrix[:, pair[0]] < order_matrix[:, pair[1]])
+    p_ba_r = np.mean(order_matrix[responses == 1, pair[0]] < order_matrix[responses == 1, pair[1]])
+    pr_ba = (p_ba_r * pr) / p_ba
+    return pr_ab, pr_ba
+
+
+def heuristic_4_features(X, y, train, test, topn=70):
+    print("heuristic 4 started")
+    order_matrix = xpressions_to_order_matrix(X[train, :])
+    value_matrix = np.zeros(shape=(order_matrix.shape[1],
+                                   order_matrix.shape[1]),
+                            dtype=np.float32)
+    for i in range(order_matrix.shape[1]):
+        for j in range(i + 1, order_matrix.shape[1]):
+            pr_ab, pr_ba = get_pair_value(order_matrix, (i, j), y[train])
+            value_matrix[i, j] = pr_ab
+            value_matrix[j, i] = pr_ba
+    if i % 5000 == 0:
+        print("\tcomplete: {} rows".format(str(i)))
+    new_val = value_matrix + 0.
+    for i in range(order_matrix.shape[1]):
+        for j in range(order_matrix.shape[1]):
+            if np.isnan(value_matrix[i, j]):
+                new_val[i, j] = 0.
+                new_val[j, i] = 0.
+    dif_matrix = new_val - new_val.T
+    p = np.percentile(dif_matrix, 99.99)
+    pairs_val = []
+    for i in range(order_matrix.shape[1]):
+        for j in range(order_matrix.shape[1]):
+            if dif_matrix[i, j] >= p:
+                pairs_val.append(((i, j), dif_matrix[i, j]))
+    pairs_val = sorted(pairs_val, key=lambda p_v: -p_v[1])
+    good_pairs = [p_v[0] for p_v in pairs_val[:topn]]
+    order_features, pair_names = xpression_to_order_feature(X, good_pairs, np.arange(X.shape[1]))
+    description = "\t".join(pair_names)
+    return order_features, description
+
+
+def heuristic_4_rnd(X, y, train, test, topn=70):
+    print("heuristic 4 started")
+    good_pairs = []
+    choosen = np.random.choice(np.arange(X.shape[1]), size=2 * topn, replace=False)
+    for i in range(0, 2 * topn, 2):
+        good_pairs.append((choosen[i], choosen[i + 1]))
+    order_features, pair_names = xpression_to_order_feature(X, good_pairs, np.arange(X.shape[1]))
+    description = "\t".join(pair_names)
+    return order_features, description
+
+
+def identity_heuristic(X, y, train, test):
+    return X, 'identity'
+
+
+def rnd_gen_heuristic(X, y, train, test, topn=250):
+    g = np.arange(X.shape[0])
+    np.random.shuffle(g)
+    return X[:, g], 'rnd'
+
+
+def heuristics_3(X, y, train, test, topn=250):
+    print('start M3')
+    resp_mask = y[train] == 1
+    nonresp_mask = y[train] == 0
+    gen_pdfs = []
+    nb_bins = 15
+    for g in range(X[train].shape[1]):
+        hists = get_gen_hists(X[train][:, g], [resp_mask, nonresp_mask], bins=nb_bins)
+        p = (hists[0] + 1) / (hists[0].sum() + hists[0].shape[0])
+        q = (hists[1] + 1) / (hists[1].sum() + hists[1].shape[0])
+        gen_pdfs.append((p, q))
+    gen_pdf_sims = []
+    for gen_pdf in gen_pdfs:
+        gen_pdf_sims.append(get_pdf_sim(gen_pdf[0], gen_pdf[1]))
+    gen_pdf_sims = np.array(gen_pdf_sims)
+    good_h3 = get_n_biggest_sims_ids(gen_pdf_sims, np.arange(X.shape[0]), 250)
+    print('complete')
+    return X[:, good_h3], 'heuristic 3'
---
+++ src/util.py
@@ -0,0 +1,167 @@
+import numpy as np
+import pandas as pd
+import os
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import confusion_matrix, roc_curve, auc
+from scipy import stats
+
+
+
+
+def parse_response(str_id):
+    if str_id.split('_')[1] == 'resp':
+        return 1
+    return 0
+
+
+def preprocess_dataset(dataset):
+    gen_names = dataset["SYMBOL"].as_matrix()
+    responses = np.zeros(dataset.shape[1] - 1, dtype=np.int)
+    target_ids = dataset.columns[1:].values
+    feature_matrix = np.zeros((dataset.shape[1] - 1, dataset.shape[0]), dtype=np.float)
+    for idx, target_id in enumerate(target_ids):
+        feature_matrix[idx, :] = dataset[target_id].values
+        responses[idx] = parse_response(target_id)
+    return feature_matrix, responses, target_ids, gen_names
+
+
+# def get_relation_matrix(order_seq):
+#     matrix = np.zeros(shape=(order_seq.shape[0], order_seq.shape[0]), dtype=np.int32)
+#     for row in range(matrix.shape[0]):
+#         for col in range(matrix.shape[1]):
+#             matrix[row, col] = order_seq[row] - order[col]
+#     return matrix
+
+
+# def get_relation_cumul_matrix(order_matrix):
+#     matrix = np.zeros(shape=(order_matrix.shape[1], order_matrix.shape[1]), dtype=np.float32)
+#     for row in range(matrix.shape[1]):
+#         for col in range(matrix.shape[1]):
+#             matrix[row, col] = (order_matrix[:, row] - order_matrix[:, col]).mean()
+#         if row % 1000 == 0:
+#             print(row)
+#     return matrix
+
+
+def get_metrics(conf):
+    """return tuple (precision 0 class, recall 0 class, precision 1 class, recall 1 class)"""
+    prec_0 = conf[0, 0] / (conf[1, 0] + conf[0, 0])
+    if (conf[1, 0] + conf[0, 0]) == 0:
+        prec_0 = 0
+    recall_0 = conf[0, 0] / (conf[0, 0] + conf[0, 1])
+    prec_1 = conf[1, 1] / (conf[1, 1] + conf[0, 1])
+    recall_1 = conf[1, 1] / (conf[1, 1] + conf[1, 0])
+    if (conf[1, 1] + conf[0, 1]) == 0:
+        prec_1 = 0
+    accuracy = (conf[0, 0] + conf[1, 1]) / (conf.sum())
+    return prec_0, recall_0, prec_1, recall_1
+
+
+def gen_samples(X, y, iters=10, cv=10, bootstrap_max=False):
+    samples = []
+    for it in range(iters):
+        subsample = get_balanced_subsample(y, bootstrap_max=bootstrap_max)
+        skfold = StratifiedKFold(n_splits=cv, shuffle=True)
+        skfold.get_n_splits(X[subsample], y[subsample])  # для совместимости с предыдущей версией
+        samples.append([(subsample[train], subsample[test])
+                        for train, test in skfold.split(X[subsample], y[subsample])])
+    return samples
+
+
+def get_balanced_subsample(y, bootstrap_max=False):
+    assert np.unique(y).shape[0] == 2
+    nonresp = np.arange(y.shape[0])[y == 0]
+    resp = np.arange(y.shape[0])[y == 1]
+    if not bootstrap_max:
+        class_size = min(nonresp.shape[0], resp.shape[0])
+        return np.hstack((nonresp[:class_size], resp[:class_size]))
+    class_size = max(nonresp.shape[0], resp.shape[0])
+    nr_addition = np.random.choice(nonresp, size=class_size - nonresp.shape[0])
+    r_addition = np.random.choice(resp, size=class_size - resp.shape[0])
+    return np.hstack((nonresp, nr_addition, resp, r_addition))
+
+
+def do_simmetry_class_cv(X, y, samples, clf, feature_maker):
+    results_a, results_b, cv_results, accs = [], [], [], []
+    all_descriptions = []
+    for ind, sample in enumerate(samples):
+        print("iteration: {}".format(ind))
+        stat, common_stat, cv_result, descriptions = do_cross_val(X, y, sample,
+                                                                  clf, feature_maker)
+        results_a.append(stat.mean(axis=0))
+        results_b.append(np.array(common_stat))
+        cv_results.append(cv_result)
+        all_descriptions.append(descriptions)
+    return np.vstack(results_a), np.vstack(results_b), cv_results, all_descriptions
+
+
+def do_cross_val(X, y, sample, clf, feature_maker):
+    cv_results, descriptions = [], []
+    for train, test in sample:
+        features, description = feature_maker(X, y, train, test)
+        descriptions.append(description)
+        clf.fit(features[train, :], y[train])
+        prediction = clf.predict(features[test, :])
+        proba = clf.predict_proba(features[test, :])
+        cv_results.append((y[test], prediction, proba[:, 1]))
+    conf_m = confusion_matrix(cv_results[0][0], cv_results[1][0]) * 0
+    stat, fpr_tpr, tpr, accs = [], [], [], []
+    for actual, predicted, proba in cv_results:
+        conf = confusion_matrix(actual, predicted)
+        stat.append(np.array(get_metrics(conf)))
+        conf_m = conf_m + conf
+    return np.vstack(stat), get_metrics(conf_m), cv_results, descriptions
+
+
+def process_results(a, b, cv_results):
+    a_aucs, a_accs, b_aucs, b_accs = [], [], [], []
+    for result in cv_results:
+        actuals, preds, probs = [], [], []
+        for actual, predicted, proba in result:
+            a_accs.append((actual == predicted).mean())
+            actuals.append(actual)
+            preds.append(predicted)
+            probs.append(proba)
+            fpr, tpr, _ = roc_curve(actual, proba)
+            a_aucs.append(auc(fpr, tpr))
+        b_accs.append((np.hstack(actuals) == np.hstack(preds)).mean())
+        fpr, tpr, thresh = roc_curve(np.hstack(actuals), np.hstack(probs))
+        b_aucs.append(auc(fpr, tpr))
+    columns = ["precision 0", "recall 0", "precision 1", "recall 1", "accuracy", "auc"]
+    rows = ["схема а", "схема б"]
+    data = np.zeros((len(rows), len(columns)))
+    data[0, :4] = a.mean(axis=0)
+    data[0, 4:] = [np.mean(a_accs), np.mean(a_aucs)]
+    data[1, :4] = b.mean(axis=0)
+    data[1, 4:] = [np.mean(b_accs), np.mean(b_aucs)]
+    table = pd.DataFrame(data, columns=columns, index=rows)
+    return table
+
+
+def read_GEO_dataset(pathto_xpr, pathto_resp, pathto_nonresp, prefix=""):
+    geo_dataset = pd.read_csv(os.path.join(prefix, pathto_xpr), sep=" ")
+    with open(os.path.join(prefix, pathto_resp), "r") as respf, \
+            open(os.path.join(prefix, pathto_nonresp), "r") as nonrespf:
+        resp = set(respf.read().split())
+        nonresp = set(nonrespf.read().split())
+    responses = np.array(list(map(lambda x: 1 if x in resp else int(x in nonresp) - 1,
+                                  geo_dataset.columns[1:])))
+    feature_matrix = geo_dataset[geo_dataset.columns[1:]].get_values().T
+    target_ids = geo_dataset.columns[1:].values
+    gen_names = geo_dataset["Group.1"].as_matrix()
+    return feature_matrix, responses, target_ids, gen_names
+
+
+def test_auc_greater(cv_0, cv_1):
+    aucs_0 = []
+    aucs_1 = []
+    for result in cv_0:
+        for actual, predicted, proba in result:
+            fpr, tpr, _ = roc_curve(actual, proba)
+            aucs_0.append(auc(fpr, tpr))
+    for result in cv_1:
+        for actual, predicted, proba in result:
+            fpr, tpr, _ = roc_curve(actual, proba)
+            aucs_1.append(auc(fpr, tpr))
+    aucs_0, aucs_1 = np.array(aucs_0), np.array(aucs_1)
+    return stats.mannwhitneyu(aucs_0, aucs_1, alternative='less').pvalue
